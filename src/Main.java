e65120872b1e20a41140c3a301873d9b990edef6
RashedNabil <85712609+RashedNabil@users.noreply.github.com>
2023-08-09 15:13:47 +1000

    Week 2
    late commit for week 2, Committing late because somehow my commit during week 2 practical class didnot go through

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Main window = new Main();
+        });
+    }
+
+    class Canvas extends JPanel {
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 720));
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            // Set the cell size and offset values
+            int cellSize = 35;
+            int xOffset = 10;
+            int yOffset = 10;
+
+            // Draw the grid
+            for (int x = 0; x < 20; x++) {
+                for (int y = 0; y < 20; y++) {
+                    int xPos = xOffset + x * cellSize;
+                    int yPos = yOffset + y * cellSize;
+
+                    g.drawRect(xPos, yPos, cellSize, cellSize);
+                }
+            }
+        }
+    }
+
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
     }
 }
